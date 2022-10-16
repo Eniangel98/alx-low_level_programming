4f1dc8370eb94855af498bb5d8a869d4527d9051
Eniangel98 <bamidelebeatrice048@gmail.com>
2022-10-16 13:10:21 -0700

    My banner

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- *main-program entry point.
- *Return:0 success, non zero error.
+ * main - entry point
+ * Description: print all possible combinations of single-digit numbers
+ * Return: 0 (Correct)
  */
 int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0'; <= '9'; num++)
 	{
 		putchar(num);
 		if (num == '9')
@@ -16,8 +18,8 @@ int main(void)
 		}
 		else
 		{
-			putchar(',');
-			putchar('');
+			putchar(44);
+			putchar(32);
 		}
 	}
 	putchar('\n');
