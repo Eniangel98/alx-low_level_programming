0990bb90d1d593ab644a224e40afbbf16bb18d58
Eniangel98 <bamidelebeatrice048@gmail.com>
2022-10-18 13:21:48 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,6 +7,7 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
+
 	char letter;
 
 	while (count++ <= 9)
@@ -15,3 +16,4 @@ void print_alphabet_x10(void)
 			_putchar(letter);
 		_putchar('/n');
 	}
+}
