fc6afc13bc578110c9272bb695a71ebf2d1f8ad2
Eniangel98 <bamidelebeatrice048@gmail.com>
2022-10-20 01:30:55 -0700

    Task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
-
+int convert_day(int month, int day);
 
 #endif
